f957743ef0ad678252ee4237baec534564458ebf
Phil <waldi_92@hotmail.com>
2015-10-28 16:50:50 +0100

    eventlistcontroller erzeugen

--- webapp/source/classes/main.js
+++ webapp/source/classes/main.js
@@ -4,18 +4,22 @@
 require.config({
     // base url relative to the index.html
     baseUrl: './',
-    paths: {
-        'angular': 'frameworks/angular/angular.min'
+    path:{
+        'frameworks/angular': 'frameworks/angular/angular.min', 'app': 'classes'
     },
-    // angular does not support async loading out of the box -> use the shim loader
-    shim: {
-        'angular': {
+    shim:{
+        'frameworks/angular':{
             exports: 'angular'
         }
     }
 });
 
-define(['angular'], function (angular) {
+define(['frameworks/angular'], function (angular) {
     var app = angular.module("Eventology");
     return angular.bootstrap(app);
+});
+
+//We need to
+define(['frameworks/angular', 'app/modules/Eventology'], function (Angular, Eventology){
+    return Angular.bootstrap(Eventology);
 });
--- webapp/source/classes/modules/Eventology.js
+++ webapp/source/classes/modules/Eventology.js
@@ -1,3 +1,12 @@
 /**
  * Created by Phil on 28.10.2015.
  */
+// declare dependency to angular (similar to import in java)
+define(['frameworks/angular'], function (Angular) {
+
+    // Create new empty app/module named 'lafete'
+    var Eventology = Angular.module('Eventology', []);
+
+    // export module to use it in other classes
+    return Eventology;
+});
