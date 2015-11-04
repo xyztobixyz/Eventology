5b8d990682d946f4283f61be2a7d54d0a634c04b
Phil <waldi_92@hotmail.com>
2015-11-04 16:01:08 +0100

    eventcontroller

--- webapp/source/classes/controllers/EventListController.js
+++ webapp/source/classes/controllers/EventListController.js
@@ -1,10 +1,10 @@
 /**
  * Created by Phil on 28.10.2015.
  */
-function EventListController() {
-    events[
-        {name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z')},
-        {name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z')},
-        {name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z')}
-        ]
-};
+var Eventology=angular.module('Eventology', []);
+
+Eventology.controller('EventListController',['$scope',function($scope){
+    $scope.events=  {name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z')},
+                    {name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z')},
+                    {name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z')}
+}]);
