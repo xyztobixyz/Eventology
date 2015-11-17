0d9f38b8e45e2960cacc789d945ba2b059a9abf2
Phil <waldi_92@hotmail.com>
2015-11-17 10:38:38 +0100

    igv

--- webapp/source/classes/controllers/EventListController.js
+++ webapp/source/classes/controllers/EventListController.js
@@ -7,7 +7,7 @@
             this.scope = $scope;
             this.scope.events=[
                 {name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z')},
-                {name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z')},
+                {name: 'Dinner', place: 'Zurich', date: new Date('2015-04-05T16:00:00.000Z')},
                 {name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z')}
             ];
         };
