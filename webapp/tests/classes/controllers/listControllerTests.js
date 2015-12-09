1c3435f9f87c42753a2872baaf86c20b9ada8dfa
Phil <waldi_92@hotmail.com>
2015-12-09 11:54:21 +0100

    tests

--- webapp/tests/classes/controllers/listControllerTests.js
+++ webapp/tests/classes/controllers/listControllerTests.js
@@ -10,14 +10,14 @@ define(['app/controllers/eventListController', 'app/model/event', 'frameworks/an
                     events.push(EventModel.createFromDTO({
                         id: "1234567891",
                         name: "TestEvent",
-                        description: "Eine Beschreibung so schön wie nichts!",
-                        targetGroup: "WOW",
-                        contributionDescription: "Mitbringen was zu haben ist",
+                        description: "Eine Beschreibung so schï¿½n wie nichts!1",
+                        targetGroup: "WOW1",
+                        contributionDescription: "Mitbringen was zu haben ist1",
                         location: {
-                            name: "Test",
-                            street: "eineStr",
-                            zipCode: 1234,
-                            city: "Stadt" },
+                            name: "Test1",
+                            street: "Teststr 1",
+                            zipCode: 1111,
+                            city: "Stadt1" },
                         maximalAmountOfGuests: 10,
                         times: {
                             begin: "2015-12-12T12:12:00Z",
@@ -28,14 +28,14 @@ define(['app/controllers/eventListController', 'app/model/event', 'frameworks/an
                     events.push(EventModel.createFromDTO({
                         id: "1234567892",
                         name: "TestEvent2",
-                        description: "Eine Beschreibung so schön wie nichts!2",
-                        targetGroup: "WOW!!",
-                        contributionDescription: "Mitbringen was zu haben ist",
+                        description: "Eine Beschreibung so schï¿½n wie nichts!2",
+                        targetGroup: "WOW2",
+                        contributionDescription: "Mitbringen was zu haben ist2",
                         location: {
-                            name: "Tes1123t",
-                            street: "feineStr",
-                            zipCode: 2234,
-                            city: "Stadt123" },
+                            name: "Test2",
+                            street: "Teststr 2",
+                            zipCode: 2222,
+                            city: "Stadt2" },
                         maximalAmountOfGuests: 15,
                         times: {
                             begin: "2015-12-12T12:12:00Z",
@@ -46,14 +46,14 @@ define(['app/controllers/eventListController', 'app/model/event', 'frameworks/an
                     events.push(EventModel.createFromDTO({
                         id: "1234567893",
                         name: "TestEvent3",
-                        description: "Eine Beschreibung so schön wie nichts!234",
-                        targetGroup: "WOWFuu",
-                        contributionDescription: "Mitbringen was zu haben ist oder nichts",
+                        description: "Eine Beschreibung so schï¿½n wie nichts!3",
+                        targetGroup: "WOW3",
+                        contributionDescription: "Mitbringen was zu haben ist3",
                         location: {
-                            name: "Test23",
-                            street: "keineStr",
-                            zipCode: 1239,
-                            city: "Sta345dt" },
+                            name: "Test3",
+                            street: "Teststr 3",
+                            zipCode: 3333,
+                            city: "Stadt3" },
                         maximalAmountOfGuests: 100,
                         times: {
                             begin: "2015-12-12T12:12:00Z",
@@ -79,21 +79,69 @@ define(['app/controllers/eventListController', 'app/model/event', 'frameworks/an
                 });
 
                 it('event names', function(){
-                    expect(eventListController.scope.events[0].name).toBe("TestEvent");
+                    expect(eventListController.scope.events[0].id).toBe("1234567891");
+                    expect(eventListController.scope.events[1].id).toBe("1234567892");
+                    expect(eventListController.scope.events[2].id).toBe("1234567893");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].name).toBe("TestEvent1");
                     expect(eventListController.scope.events[1].name).toBe("TestEvent2");
                     expect(eventListController.scope.events[2].name).toBe("TestEvent3");
                 });
 
                 it('event names', function(){
-                    expect(eventListController.scope.events[0].name).toBe("TestEvent");
-                    expect(eventListController.scope.events[1].name).toBe("TestEvent2");
-                    expect(eventListController.scope.events[2].name).toBe("TestEvent3");
+                    expect(eventListController.scope.events[0].description).toBe("Eine Beschreibung so schÃ¶n wie nichts!1");
+                    expect(eventListController.scope.events[1].description).toBe("Eine Beschreibung so schÃ¶n wie nichts!2");
+                    expect(eventListController.scope.events[2].description).toBe("Eine Beschreibung so schÃ¶n wie nichts!3");
                 });
 
                 it('event names', function(){
-                    expect(eventListController.scope.events[0].name).toBe("TestEvent");
-                    expect(eventListController.scope.events[1].name).toBe("TestEvent2");
-                    expect(eventListController.scope.events[2].name).toBe("TestEvent3");
+                    expect(eventListController.scope.events[0].targetGroup).toBe("WOW1");
+                    expect(eventListController.scope.events[1].targetGroup).toBe("WOW2");
+                    expect(eventListController.scope.events[2].targetGroup).toBe("WOW3");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].contributionDescription).toBe("Mitbringen was zu haben ist1");
+                    expect(eventListController.scope.events[1].contributionDescription).toBe("Mitbringen was zu haben ist2");
+                    expect(eventListController.scope.events[2].contributionDescription).toBe("Mitbringen was zu haben ist3");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].location.name).toBe("Test1");
+                    expect(eventListController.scope.events[1].location.name).toBe("Test2");
+                    expect(eventListController.scope.events[2].location.name).toBe("Test3");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].location.street).toBe("Teststr 1");
+                    expect(eventListController.scope.events[1].location.street).toBe("Teststr 2");
+                    expect(eventListController.scope.events[2].location.street).toBe("Teststr 3");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].location.zipCode).toBe("1111");
+                    expect(eventListController.scope.events[1].location.zipCode).toBe("2222");
+                    expect(eventListController.scope.events[2].location.zipCode).toBe("3333");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].location.city).toBe("Stadt1");
+                    expect(eventListController.scope.events[1].location.city).toBe("Stadt2");
+                    expect(eventListController.scope.events[2].location.city).toBe("Stadt3");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].times.begin).toBe("2015-12-12T12:12:00Z");
+                    expect(eventListController.scope.events[1].times.begin).toBe("2015-12-12T12:12:00Z");
+                    expect(eventListController.scope.events[2].times.begin).toBe("2015-12-12T12:12:00Z");
+                });
+
+                it('event names', function(){
+                    expect(eventListController.scope.events[0].times.end).toBe("2016-12-12T12:12:00Z");
+                    expect(eventListController.scope.events[1].times.end).toBe("2016-12-12T12:12:00Z");
+                    expect(eventListController.scope.events[2].times.end).toBe("2016-12-12T12:12:00Z");
                 });
             });
         });
